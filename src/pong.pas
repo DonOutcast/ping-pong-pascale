5f2f68e9aba4c8a661c2e8d4caa5b42796394823
DonOutcast <sham1996@yandex.ru>
2022-07-02 12:09:58 +0300

    d

---
+++ src/pong.pas
@@ -0,0 +1,33 @@
+program pin_pong;
+uses
+        Crt;
+const
+        title = 'Ping Pong with Free Pascal';
+        width = 27;
+        height = 82;
+        rocket_left = 2;
+        rocket_right = 24;
+var
+        i, j  : integer;
+begin
+        ClrScr;
+    for i := 1 to width do
+    begin
+            for j := 1 to height do
+                    begin
+            if (i = width ) or (i = 2) and (j <> 1) and (j <> height) then
+                    write('*')
+            else if (i = 1) and (j =22) then
+                write(title)
+            else if (j = 1) or (j = height )  and (i <> 1) and (i <> width ) then
+                    write('*')
+            else if (j = 41) and (i <> 1) and (i <> height) then
+                    write('|')
+            else if (j = rocket_left) and (i <> 1) then
+                    write('|')
+            else
+                write(' ');
+            end;
+            writeln('');
+    end
+end.
